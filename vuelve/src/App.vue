d641fefcd99284b3fe62151cd155982820d81d12
Anthony Cassidy <githuba2g@antcassidy.com>
2023-04-29 11:10:15 +1200

    this doesn't work

--- vuelve/src/App.vue
+++ vuelve/src/App.vue
@@ -1,13 +1,20 @@
 <script>
-
 import Example from './components/Example.vue'
 import Tree from './components/Tree.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  
   components: {
     Example,
-    Tree
+    Tree,
+    Modal
+  },
+  data() {
+    return {
+      showModal: false
+    }
   }
 }
 </script>
@@ -15,8 +22,17 @@ export default {
 <template>
   <div id="app">
     <h1>Title is set in App.vue </h1>
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
     <tree />
     <example />
+    <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>custom header</h3>
+      </template>
+    </modal>
+  </Teleport>
   </div>
 </template>
 
