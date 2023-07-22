e1c6d0efe4029ef43e5ae7ae68c4aa6a0f3149f9
Anthony Cassidy <githuba2g@antcassidy.com>
2023-07-23 10:42:36 +1200

    Omitted tests fromconfig ts
    npm run test -t GenerateMapOfLeavesRecursively

--- testsYup/test/puzzle/GenerateMapOfLeavesRecursively.test.ts
+++ testsYup/test/puzzle/GenerateMapOfLeavesRecursively.test.ts
@@ -1,11 +1,12 @@
 import { GenerateMapOfLeavesRecursively } from '../../../src/puzzle/GenerateMapOfLeavesRecursively';
 import { Piece } from '../../../src/puzzle/Piece';
 
-test('Test GetMapOfAllStartingThings', () => {
+test('GenerateMapOfLeavesRecursively', () => {
   const map = new Map<string, Piece | null>();
   // eslint-disable-next-line no-return-assign, no-param-reassign
   const piece = new Piece(1, null, 'theOutput', '', 1, null, null, 'a', 'b'); // eslint-disable-line no-return-assign, no-param-reassign
   GenerateMapOfLeavesRecursively(piece, '', map);
 
-  expect(map.size).toEqual(1);
+  expect(map.size).toBe(3);
+  expect(map.get('/a')).toBe(null);
 });
